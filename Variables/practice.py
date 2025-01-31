3bf9ddf2abdaad80d0c76cdb836bf2e53f2bcbd2
zosucks <zoe.jimenez@ucas-edu.net>
2025-01-31 11:33:54 -0700

    Variables practice - python

--- Variables/practice.py
+++ Variables/practice.py
@@ -1,3 +1,5 @@
+#Zoe Jimenez, Variable
+
 name = "Zoe"
 
 number1 = 9
@@ -12,3 +14,13 @@ school = "UCAS"
 
 year = 2025
 
+eyecolor = "brown"
+
+age = 14
+
+favsubject = "math"
+
+print(name, "really likes", favsubject, "at the school", school, "shes also the age", age, "meaning her grade is", number1,"th")
+print("a random number that", name, "thought of was", number2)
+print(name, "eats", breakfast, "while she looks around with her", eyecolor, "eyes")
+print(name, "favorite color is", favcolor, name, "did this assignment in", year)
