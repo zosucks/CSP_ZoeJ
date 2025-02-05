530b3d58992fe8c18d2e124d81e15cf209ab059a
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-05 01:34:05 +0000

    first program C

---
+++ Variables/firstprogram.c
@@ -0,0 +1,16 @@
+// Zoe jimenez, first program C
+#include <stdio.h>
+
+char name[];
+int age;
+
+
+int main(void){
+    printf("welcome, what is your name: \n");
+    scanf("%s", name);
+    printf("whats your age: \n");
+   scanf("%d", age);
+    printf("Hello %s! you are %f years old!\n", name, age);
+
+    return 0;
+}
