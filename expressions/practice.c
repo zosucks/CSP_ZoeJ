c2eaaecebf75338203de97e16933478d8c01bfe1
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-06 02:03:11 +0000

    half of practice.c

--- expressions/practice.c
+++ expressions/practice.c
@@ -1,1 +1,17 @@
 // Zoe Jimenez, Expression practice C
+
+#include <stdio.h>
+#include <math.h>
+
+int equation1 = 7-24/8*4+6;
+int equation2 = 18/3-7+2*5; 
+int equation3 = 6*4;
+int equation3a = equation3/12;
+
+
+
+int main(void){
+    printf("%d, %d, %d", equation1, equation2, equation3a);
+
+    return 0;
+}
