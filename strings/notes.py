c96ab04132c4c884b1f3c3869f5e83a70d85216d
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-10 11:25:00 -0700

    new notes

---
+++ strings/notes.py
@@ -0,0 +1,9 @@
+# Zoe jimenez, strings notes python
+
+# string is a data type that holds information surrounded by quotation marks "" ''
+
+note = "Zoe's seat"
+
+
+
+sentence = "the quick brown fox jumps over the lazy dog"
