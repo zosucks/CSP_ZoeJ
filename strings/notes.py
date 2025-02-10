738fa5592e0c6c5e4795a8601222b55baeaca8f5
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-10 11:49:13 -0700

    notes

--- strings/notes.py
+++ strings/notes.py
@@ -4,6 +4,10 @@
 
 note = "Zoe's seat"
 
-
-
+#name = input("what is your first name").strip().capitalize()
+#print(f"Hello {name}, welcome to program")
 sentence = "the quick brown fox jumps over the lazy dog"
+
+print(note)
+
+name = input("what is you name?")
