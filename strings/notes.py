79a08ec4e68c7759b4def1712e6e721f0a09ad76
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-11 11:16:11 -0700

    python notes

--- strings/notes.py
+++ strings/notes.py
@@ -7,7 +7,18 @@ note = "Zoe's seat"
 #name = input("what is your first name").strip().capitalize()
 #print(f"Hello {name}, welcome to program")
 sentence = "the quick brown fox jumps over the lazy dog"
+print(sentence)
+print(len(sentence))
+# above this reads how many charcters are in a string, aswell spaces.
+print(sentence[4])
+print(sentence.find("brown"))
+print(sentence[10:19])
+start = sentence.find("brown")
+length = len("brown fox")
+
 
 print(note)
 
 name = input("what is you name?")
+print("this is your name: "+ name)
+#this is incatination, above
