f302ba893340c7d0eb7447db0766a3ee6de9b7af
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-12 11:48:58 -0700

    name C

--- strings/name_decorator.c
+++ strings/name_decorator.c
@@ -1,2 +1,20 @@
 // Zoe Jimenez, Name Decorator C
 
+
+#include <stdio.h>
+char name[55];
+char one = ("###");
+char three = ("###");
+
+int main(void){
+    printf("Hellow user this is a program to decorate your name\n");
+    printf("What is your name?: \n");
+    scanf("%d", name);
+
+
+strcat(one, name);
+strcat(one, three);
+
+
+    return 0;
+}
