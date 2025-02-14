c162cc521fe320625962dbf976a7f46b9660c06b
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-14 02:56:17 +0000

    ss python

--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 // empty string vairables for user words (min 3)
     char color[20];
-    char state[35];
+    char state[20];
     float num;
     float num1;
-    char color1[35];
-    char shape[50];
-    char shape1[50];
+    char color1[20];
+    char shape[20];
+    char shape1[20];
 
 
 
@@ -19,29 +19,29 @@ int main(void){
 
     // ask user for words (print statment with a question, scanf to set to variable)
     printf("Please name one color: \n");
-    scanf("%d", color);
+    scanf("%s", &color);
 
     printf("Please name a SINGLE WORD state: \n");
-    scanf("%d", state);
+    scanf("%s", &state);
 
     printf("Please type in a solid number: \n");
-    scanf("%s", num);
+    scanf("%d", &num);
 
     printf("Please type in another solid number: \n");
-    scanf("%s", num1);
+    scanf("%d", &num1);
 
     printf("Please type another a ONE WORD color: \n");
-    scanf("%d", color1);
+    scanf("%s", &color1);
 
     printf("Please name a ONE WORD shape: \n");
-    scanf("%d", shape);
+    scanf("%s", &shape);
 
     printf("Please name another ONE WORD shape: \n");
-    scanf("%d", shape1);
+    scanf("%s", &shape1);
     // make sure to tell user to put in only one word.
 
     //print out the story with the variables inserted
-    printf("A %d spaceship landed in %d. Can you believe that %s aliens got off the ship! They had %d eyes and two %d ")
+    printf("A %s spaceship landed in %s. Can you believe that %d aliens got off the ship! They had %d eyes and two %s mouths! They had %s heads and %s noses! \n", color, state, num, num1, color1, shape, shape1);
     // all in one print statement!!
     
     return 0;
--- strings/silly_sentences.py
+++ strings/silly_sentences.py
@@ -1,1 +1,13 @@
 # Zoe Jimenez, silly sentences PYTHON
+
+print("Hello user this is a program to make silly sentences!")
+
+color = input("Please type a  color: \n")
+state = input("Please type in a SINGLE WORD state: \n")
+num = input("Please type a SOLID number: \n")
+num1 = input("Please write another SOLID number: \n")
+color1 = input("Please type a color name: \n")
+shape = input("Please type in a shape name: \n")
+shape1 = input("Please type another shape name: \n")
+
+print("A", color,"spaceship landed in", state,"! Can you belive that", num,"aliens got off the ship! They had", num1,"eyes and two", color1,"mouths! They had", shape,"heads and", shape1,"noses!")
