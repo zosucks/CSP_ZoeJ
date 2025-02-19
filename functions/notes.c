0ce7210c28cb02d423b458832aecdd6811ff97ed
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-19 11:48:51 -0700

    notes

--- functions/notes.c
+++ functions/notes.c
@@ -6,6 +6,9 @@ int add(int numOne, int numTwo){
     return numOne + numTwo;
 
 }
+void due(char assignment[50], char day[20]){
+    printf("The %s assignment is due %s", assignment, day);
+}
 
 const char* word(char type[50]){
     char choice[50];
@@ -22,10 +25,11 @@ int main(void){
     //scanf("%d", num);
     //add(num,1);
     //add(8,1);
-    add(72,1);
-printf("%d",add);
-
-
+    //add(72,1);
+    //printf("%d",add);
+    due("Functions notes", "Today");
+    due("Hello world update", "Tommorrow");
+    due("Fincanial calculator Update", "Friday");
 
 
 
