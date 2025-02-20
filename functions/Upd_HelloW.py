0b8befe6cac47647335030d53d692e7d86c88204
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-20 11:46:16 -0700

    helloworld

--- functions/Upd_HelloW.py
+++ functions/Upd_HelloW.py
@@ -1,2 +1,13 @@
 #update hello world, Zoe Jimenez
 
+def info(type):
+    name = input("What is your name?: \n")
+    print(f"Hello {name}, welcome to programming!")
+
+
+
+info("name")
+info("name")
+info("name")
+info("name")
+info("name")
