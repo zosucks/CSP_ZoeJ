027fd9b6d5c0f450ab52d05f9c5e8227eca27f88
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-24 02:03:59 +0000

    i cede

--- functions/update_calc.py
+++ functions/update_calc.py
@@ -1,39 +1,48 @@
 # Zoe jimenez, UPDATE Financial calculator PYTHON
-def info(cost, income, type):
 
-    print(f"Your {type} is {cost:.2f} Which is {percent}% of your income.\n" )
-
-def present(cost, income):
-    percent = (cost/income)*100
+print("Welcome User! This is a financial Calculator for your needs.")
+def user_inputs(type):
     
+    input(f"What is your {type}?: ")
+ 
+income = user_inputs("income")
+rent = user_inputs("rent")
+utilities = user_inputs("utilities")
+groceries = user_inputs("groceries")
+transportation = user_inputs("transportion")
+
+
 
 # print statment that welcomes user and tells what this program does
-print("Welcome User! This is a financial Calculator for your needs.")
+#print("Welcome User! This is a financial Calculator for your needs.")
 # ask what their monthly income is (variable an input)
-income = float(input("What is your income?: \n"))
+#income = float(input("What is your income?: \n"))
 # ask what their rent is (variable an input)
-rent = float(input("What is your rent?\n"))
+#rent = float(input("What is your rent?\n"))
 # ask what their utilities cost is (variable an input)
-utilities = float(input("What is your utilities cost?\n"))
+#utilities = float(input("What is your utilities cost?\n"))
 # ask what their  grocories cost is (variable an input)
-groceries = float(input("What is your groceries cost?\n"))
+#groceries = float(input("What is your groceries cost?\n"))
 # ask what their transportation cost is (variable an input)
-transportation = float(input("What is your transportation cost?\n"))
-saving = income*.1
-savings = (saving/income)*100
-spending = income-saving-rent-groceries-utilities-transportation
-spendings = (spending/income)*100
+#transportation = float(input("What is your transportation cost?\n"))
+
+def info(cost, income, type):
+    percent = (cost/income)*100
+    print(f"Your {type} is {cost:.2f} Which is {percent}% of your income.\n" )
+
+
+
+
+#saving = income*.1
+#savings = (saving/income)*100
+#spending = income-saving-rent-groceries-utilities-transportation
+#spendings = (spending/income)*100
 
 
 info(rent, income, "rent")
 info(utilities, income, "utlities")
 info(groceries, income, "groceries")
 info(transportation, income, "transportation")
-info(savings, income, "savings")
-info(spending, income, "spending")
+#info(savings, income, "savings")
+#info(spending, income, "spending")
 
-present(rent, income)
-present(utilities, income)
-present(groceries, income)
-present(transportation, income)
-present(savings, income)
