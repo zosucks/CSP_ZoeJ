cc4bb7e85c025e75369b3b8094802f6a75234f2d
zosucks <zoe.jimenez@ucas-edu.net>
2025-02-27 11:49:09 -0700

    PLEASEE

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -3,13 +3,15 @@
 print("Hello, are you old enough?")
 age = int(input("What is your age?:\n"))
 
-if age == 6:
-   print("Youre old enough to go to school")
+
+
+if age >= 18:
+   print("Youre old enough to vote")
+elif age >= 16:
+   print("Youre old enough to drive")
 elif age == 15:
    print("Youre old enough to get your learner permit")
-elif age == 16:
-   print("Youre old enough to drive")
-elif age >= 18:
-   print("Youre old enough to vote")
+elif age == 6:
+   print("Youre old enough to go to school")
 else:
    print("Youre too young")
