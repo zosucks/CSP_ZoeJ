6a67837e9b8cdcdc9aa30366aec82cd24e91c6ad
zosucks <zoe.jimenez@ucas-edu.net>
2025-03-24 11:49:09 -0600

    final inprogess

---
+++ programming_final/pro_fin.py
@@ -0,0 +1,24 @@
+#PROGRAMMING FINAL PYTHON
+print("Welcome to hangman!")
+print("______________")
+print("             |")
+
+
+#Brahm: loop/ which word(in a list) to make the user guess
+
+
+
+
+#Kaleb: function that runs if they get the letter wrong (includes making the hangman if wrong)
+
+
+
+#Gov: the other function that runs when the user gets it correct (includes spelling the word at the bottom)
+print("")
+
+
+
+
+#Zoe: conditional runs when the user fails or wins then repeats the code
+letter = input("Please guess the word with only one letter:")
+if letter
