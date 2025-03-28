277a3e6c14daaaa58b0ccb08ea42b8b9da924a68
zosucks <zoe.jimenez@ucas-edu.net>
2025-03-28 05:17:33 +0000

    finals

--- programming_final/pro_fin.py
+++ programming_final/pro_fin.py
@@ -1,19 +1,21 @@
 #PROGRAMMING FINAL PYTHON
 #featuring Brahm, Gov, Kaleb, and Zoe
 import random
-print("Welcome to hangman!")
+print("Welcome to hangman, begin if you DAREEEE!!")
 
 
 #kaleb: function that runs when the user fails or wins then repeats the code
 def play_hangman(word):
     #Sets how many attempts user has
     attempts = 6  
+
     #Opens a variable for the going to be guessed letters
     guessed_letters = []
+
     #Then this opens the visual part of the hangman
     hangman_pictures = [
         """
-         -----
+         _______
          |   |
          |
          |
@@ -21,7 +23,7 @@ def play_hangman(word):
          |
         [][][]]""",
         """
-         -----
+         _______
          |   |
          |   O
          |
@@ -29,7 +31,7 @@ def play_hangman(word):
          |
         [][][]]""",
         """
-         -----
+         _______
          |   |
          |   O
          |   |
@@ -37,7 +39,7 @@ def play_hangman(word):
          |
         [][][]]""",
         """
-         -----
+         _______
          |   |
          |   O
          |  /|
@@ -45,7 +47,7 @@ def play_hangman(word):
          |
         [][][]]""",
         """
-         -----
+         _______
          |   |
          |   O
          |  /|\\
@@ -53,7 +55,7 @@ def play_hangman(word):
          |
         [][][]]""",
         """
-         -----
+         _______
          |   |
          |   O
          |  /|\\
@@ -61,7 +63,7 @@ def play_hangman(word):
          |
         [][][]]""",
         """
-         -----
+         _______
          |   |
          |   O
          |  /|\\
@@ -70,7 +72,7 @@ def play_hangman(word):
         [][][]]"""
     ]
 
-#Brahm: loop/ which word(in the list) to make the user guess
+#Brahm: loop that makes the whole game work as it inserts the guessed letter or not
     while attempts > 0:
         print(hangman_pictures[6 - attempts])
         display_word = ""
@@ -83,12 +85,12 @@ def play_hangman(word):
 
 
 
-# a condtional that plays when the player the user finishes the word
+#Zoe: a conditional that plays when the player the user finishes the word
         if display_word == word:
             print("Congratulations! You guessed the word!")
-            return
+            play_again()
 
-# Makes a variable for user's guesses
+#Kaleb:  a variable for user's guess
         guess = input("Guess a letter: ").lower()
 
 #Zoe: Conditional check for a valid guess
@@ -102,11 +104,23 @@ def play_hangman(word):
             guessed_letters.append(guess)
             attempts -= 1  
 
-
+#Gov: Plays when user runs out of guesses
     print(hangman_pictures[-1])
-    print(f"Game over! The word was '{word}'.")
+    print(f"Game over! The word was '{word}', better luck next time!")
+    play_again()
 
-# #Gov: the function that chooses the random word in a list
+#Zoe: Calls whole code again if user wants to play again, A function with a conditional
+def play_again():
+    again = input("Would you like to play again? (Y/N)").upper()
+    if again == "Y":
+        the_words()
+    elif again == "N":
+        exit()
+    else:
+        print("That isn't a Y or an N! Try again! :(")
+        play_again()
+
+#Gov: the function that chooses the random word in a list
 def the_words():
     words = ["software", "iconic", "dirtbike", "manager", "pepsi", "explode", "investigator", "exciting", "attitude", "computer", "cyber", "architect", "printer", "gatos", "publication", "evaluation", "rhinoceros", "pharoah", "crocodile", "alligator", "pneumonoultramicroscopicsilicovolcanoconiosis", "supercalifragilisticexpialidocious", "lebron"]
     random_words = random.choice(words)
